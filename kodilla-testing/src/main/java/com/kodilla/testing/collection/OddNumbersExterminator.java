c42911f286463b9c3dde9e673bbd084d9f041d54
Sebastian Manczak <sebmanczak@icloud.com>
2020-01-28 08:49:47 +0000

    third commit

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,11 @@
 package com.kodilla.testing.collection;
 
 public class OddNumbersExterminator {
+    public void exterminate(ArrayList<Integer> numbers) {
+        for (int i = 1; i<=100; i++){
+            if(i %2!= 0){
+                System.out.println(i)
+            }
+        }
+    }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.forum;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ForumUser {
+    private String name;
+    private String realName;
+    private ArrayList<ForumPost> posts = new ArrayList<ForumPost>();
+    private LinkedList<ForumComment> comments = new LinkedList<ForumComment>();
+
+    public ForumUser(String name, String realName) {
+
+    }
+}
