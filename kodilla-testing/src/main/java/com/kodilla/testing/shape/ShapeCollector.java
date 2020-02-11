31887eb75a26cc84574239df2184f62d225729dd
Sebastian Manczak <sebmanczak@icloud.com>
2020-02-11 16:00:35 +0000

    next

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,28 +1,29 @@
 package com.kodilla.testing.shape;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class ShapeCollector {
-    private String shapeName;
-    private ArrayList<Shape> figures = new ArrayList<Shape>();
 
-
-    public static void addFigure(String circle) {
-    }
+    private ArrayList<String> figures = new ArrayList<String>();
 
     public void addFigure(Shape shape) {
-
+        figures.add("circle");
+        figures.add("square");
+        figures.add("triangle");
     }
 
     public void removeFigure(Shape shape) {
-
+        figures.remove("circle");
     }
 
     public void getFigure(int n) {
-
+        figures.get(2);
     }
 
     public void showFigures() {
-
+        for(String all: figures) {
+            System.out.println(all);
+        }
     }
 }
