3e1bad866178ad47f3adcf80e6e545e41999ba26
Sebastian Manczak <sebmanczak@icloud.com>
2020-02-26 16:12:30 +0000

    7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,49 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        System.out.println("Let's make the text nicer.");
+        poemBeautifier.beautify("Example text", table -> "* " + table + " *" );
+        poemBeautifier.beautify("flower", xxx -> "ABC  " + xxx + "  DEF");
+        poemBeautifier.beautify("fjdifjidj", tutut -> tutut.toUpperCase());
+        poemBeautifier.beautify("RRRRRRRRRR", yy -> yy.toLowerCase());
+        poemBeautifier.beautify("RRRRRRRRRR", yy -> yy.replaceAll("R", "A"));
+
+
+
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//
+//        System.out.println("Calculating expressions with lambdas");
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Processor processor = new Processor();
+        //processor.execute(() -> System.out.println("This is an example text."));
+
+        //Processor processor = new Processor();
+        //Executor codeToExecute = () -> System.out.println("This is an example text.");
+        //processor.execute(codeToExecute);
+
+        //        Processor processor = new Processor();
+        //        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        //        processor.execute(executeSaySomething);
+
+        //        System.out.println("Welcome to module 7 - Stream");
+        //        SaySomething saySomething = new SaySomething();
+        //        saySomething.say();
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+    public void beautify(String text, PoemDecorator decorator) {   //metoda zwraca void i przyjmuje text, decorator
+        String beautifulText = decorator.decorate(text);    // zmienna
+        //  wynik dzialania funkcji, przypisalem do zmiennej wynik dzialania funkcji decorate z interfaceu PoemDecorator
+
+        System.out.println(beautifulText);
+    }
+
+}
