4b4b8928ac3349cce374e360fe8b3e2dbd2c955c
Sebastian Manczak <sebmanczak@icloud.com>
2020-04-06 20:54:17 +0100

    update

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception.test;
+import java.util.HashMap;
+
+public class FlightFinder {
+    public static void main (String[] args) {
+        Flight flight = new Flight("Dublin", "London");
+        FlightFinder flightFinder = new FlightFinder();
+        try {
+            flightFinder.findFlight(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println("There is no such flight.");
+        }
+    }
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> airports = new HashMap<>();
+        airports.put("Warsaw", false);
+        airports.put("Wroclaw", false);
+        airports.put("Dublin", false);
+
+        Boolean departureAirport = airports.get(flight.getDepartureAirport());
+        Boolean arrivalAirport = airports.get(flight.getArrivalAirport());
+
+        if (departureAirport == null || arrivalAirport == null) {
+            throw new RouteNotFoundException();
+        }
+        return arrivalAirport;
+    }
+}
