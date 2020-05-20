198ec1bc6528ce0e9e8038cc96bfcc632d3effe3
Sebastian Manczak <sebmanczak@icloud.com>
2020-05-20 23:24:48 +0100

    Spring ForumUser

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -0,0 +1,27 @@
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+    final Shape shape;
+
+    public Drawer(final Shape shape) {
+        this.shape = shape;
+    }
+    public String doDrawing() {
+        return shape.draw();
+    }
+//    public void doDrawing() {
+//        Shape shape;
+//
+//        shape = new Circle();
+//        shape.draw();
+//
+//        shape = new Triangle();
+//        shape.draw();
+
+//        Circle circle = new Circle();
+//        Triangle triangle = new Triangle();
+//
+//        circle.draw();
+//        triangle.draw();
+//    }
+}
