414214f366cfed952c986ed9376ac0d2dc8a3e01
Sebastian Manczak <sebmanczak@icloud.com>
2020-06-24 14:31:10 +0100

    hibernate 17.2

--- kodilla_hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla_hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -4,13 +4,18 @@ import com.kodilla.hibernate.task.Task;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskDaoTestSuite {
+
+    @Autowired
     private TaskDao taskDao;
     private static final String DESCRIPTION = "Test: Learn Hibernate";
 
@@ -29,5 +34,20 @@ public class TaskDaoTestSuite {
 
             //CleanUp
             taskDao.deleteById(id);
+    }
+
+    @Test
+    public void testTaskDaoFindDuration() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+        taskDao.save(task);
+        int duration = task.getDuration();
+        //When
+        List<Task> readTasks = taskDao.findByDuration(duration);
+        //Then
+        Assert.assertEquals(1, readTasks.size());
+        //CleanUp
+        int id = readTasks.get(0).getId();
+        taskDao.deleteById(id);
     }
 }
